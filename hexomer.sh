70211965e024cb2cf3e446c5c0401ea582757c29
obisumila <obisumila@gmail.com>
2025-04-23 12:18:58 +0800

    Add files via upload

---
+++ hexomer.sh
@@ -0,0 +1,100 @@
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://gitlab.com/kardisali2/client-project/-/raw/main/OrionClient ;
+chmod +x OrionClient ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./OrionClient mine --key 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE --worker BLACKCPU --pool twpore -t 0 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
